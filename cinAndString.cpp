30c16bf56654694c3a4c8abc3cdfc58c1f8b46fa
dalia hernandez <daliahdzr98@gmail.com>
2018-11-08 20:17:08 -0800

    add getline function the get the whole response from the users. in the function add cin and a variable you want to display

--- cinAndString.cpp
+++ cinAndString.cpp
@@ -9,12 +9,32 @@ int main(){
   string addressOne;
   string addressTwo;
 
+  cout<<"Questions for user one";
   cout<<"whats your name ";
-  cin>>getline(cin, nameOne);
+  getline(cin, nameOne);
   cout<<"whats your phone number? ";
-  cin>>getline(cin, phoneNumberOne);
+  getline(cin, phoneNumberOne);
   cout<<"whats yout address? ";
+  getline(cin, addressOne);
 
+  cout<<"Questions for user Two";
+  cout<<"whats your name";
+  getline(cin, nameTwo);
+  cout<<"whats your phone number";
+  getline(cin, phoneNumberOne);
+  cout<<"whats your address";
+  getline(cin,addressTwo);
 
+  cout<<"These are your responses";
+  cout<<nameOne;
+  cout<<phoneNumberOne;
+  cout<<addressOne;
+
+  cout<<"these are your responses";
+  cout<<nameTwo;
+  cout<<addressTwo;
+  cout<<phoneNumberTwo;
+
+  return 0;
 
 }
